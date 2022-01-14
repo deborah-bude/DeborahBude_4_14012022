775832eacd8ef283c684749ff0800a2f18fb6f0b
skiro <deborah.bude@gmail.com>
2022-01-14 17:18:41 +0100

    Amélioration fonction de validation de base

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,6 +18,7 @@ const lastNameInput = document.querySelector("#last");
 const numberTournamentsInput = document.querySelector("#quantity");
 
 const regexName = /[a-zA-Z]{2,}/g;
+const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g;
 const regexDate = /[0-3][0-9](\/)[0-1][0-9](\/)[1-2][0-9][0-9][0-9]/g;
 const regexNumber = /[0-9]{1,}/g;
 
@@ -41,16 +42,10 @@ function closeModal() {
 
 //Function validate first name
 function validateFirstName() {
-  console.log(firstNameInput.value.match(regexName))
-  console.log(firstNameInput.value)
-  if (firstNameInput.value == firstNameInput.value.match(regexName)) {
-    console.log('Vous avez bien écrit votre prénom ! Bravo !');
+  if (regexName.test(firstNameInput.value)) {
     formData[0].setAttribute("data-error-visible", "false");
-    if (formData[0].getAttribute("data-error") !== null || formData[0].getAttribute("data-error") !== "") {
-      formData[0].removeAttribute("data-error");
-    }
+    formData[0].removeAttribute("data-error");
   } else {
-    console.log('Hum... quelque chose ne va pas...');
     formData[0].setAttribute("data-error-visible", "true");
     formData[0].setAttribute("data-error", "Votre prénom doit comporter au minimum deux caractères avec seulement des lettres")
   }
@@ -60,9 +55,7 @@ function validateFirstName() {
 function validateLastName() {
   if (lastNameInput.value == lastNameInput.value.match(regexName)) {
     formData[1].setAttribute("data-error-visible", "false");
-    if (formData[1].getAttribute("data-error") !== null || formData[1].getAttribute("data-error") !== "") {
-      formData[1].removeAttribute("data-error");
-    }
+    formData[1].removeAttribute("data-error");
   } else {
     formData[1].setAttribute("data-error-visible", "true");
     formData[1].setAttribute("data-error", "Votre nom de famille doit comporter au minimum deux caractères avec seulement des lettres")
@@ -73,9 +66,7 @@ function validateLastName() {
 function validateNumberTournaments() {
   if (numberTournamentsInput.value == numberTournamentsInput.value.match(regexNumber)) {
     formData[4].setAttribute("data-error-visible", "false");
-    if (formData[4].getAttribute("data-error") !== null || formData[4].getAttribute("data-error") !== "") {
-      formData[4].removeAttribute("data-error");
-    }
+    formData[4].removeAttribute("data-error");
   } else {
     formData[4].setAttribute("data-error-visible", "true");
     formData[4].setAttribute("data-error", "Le nombre de tournois doit seulement être en chiffre");
