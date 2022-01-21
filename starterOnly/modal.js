c600b96c2e75ae7655cfa7589f29bc6e65c0f9de
skiro <deborah.bude@gmail.com>
2022-01-21 13:19:59 +0100

    Validation de tous les inputs sous différentes fonctions et mise en place de la fonction validate() pour confirmer l'envoie du formulaire.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,19 +7,27 @@ function editNav() {
   }
 }
 
+let validateForm= [false, false, false, false, false, false, true];
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
-//DOM Elements - 
+const form = document.querySelector("#form");
+//DOM Elements - Input
 const firstNameInput = document.querySelector("#first");
 const lastNameInput = document.querySelector("#last");
+const emailInput = document.querySelector("#email");
+const birthdayInput = document.querySelector("#birthdate");
 const numberTournamentsInput = document.querySelector("#quantity");
+const tournamentParticipationInput = document.getElementsByName("location");
+const useTermInput = document.querySelector("#checkbox1");
 
 const regexName = /[a-zA-Z]{2,}/g;
 const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g;
-const regexDate = /[0-3][0-9](\/)[0-1][0-9](\/)[1-2][0-9][0-9][0-9]/g;
+// const regexDate = /[0-3][0-9](\/)[0-1][0-9](\/)[0-9]{4}/g;
+const regexDate = /(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))/g;
 const regexNumber = /[0-9]{1,}/g;
 
 // launch modal event
@@ -27,9 +35,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 modalCloseBtn.addEventListener("click", closeModal);
 //Validate input
-firstNameInput.addEventListener("input", validateFirstName)
-lastNameInput.addEventListener("input", validateLastName)
-numberTournamentsInput.addEventListener("input", validateNumberTournaments)
+firstNameInput.addEventListener("input", validateFirstName);
+lastNameInput.addEventListener("input", validateLastName);
+emailInput.addEventListener("input", validateEmail);
+birthdayInput.addEventListener("input", validateBirthday);
+numberTournamentsInput.addEventListener("input", validateNumberTournaments);
+for (const location of tournamentParticipationInput) {
+  location.addEventListener("input", validateTournamentParticipation);
+}
+useTermInput.addEventListener("input", validateUseTerm);
 
 // launch modal form
 function launchModal() {
@@ -45,30 +59,106 @@ function validateFirstName() {
   if (regexName.test(firstNameInput.value)) {
     formData[0].setAttribute("data-error-visible", "false");
     formData[0].removeAttribute("data-error");
+    validateForm[0]= true;
   } else {
     formData[0].setAttribute("data-error-visible", "true");
-    formData[0].setAttribute("data-error", "Votre prénom doit comporter au minimum deux caractères avec seulement des lettres")
+    formData[0].setAttribute("data-error", "Votre prénom doit comporter au minimum deux caractères avec seulement des lettres.");
+    validateForm[0]= false;
   }
 }
 
 //Function validate last name
 function validateLastName() {
-  if (lastNameInput.value == lastNameInput.value.match(regexName)) {
+  if (regexName.test(lastNameInput.value)) {
     formData[1].setAttribute("data-error-visible", "false");
     formData[1].removeAttribute("data-error");
+    validateForm[1]= true;
   } else {
     formData[1].setAttribute("data-error-visible", "true");
-    formData[1].setAttribute("data-error", "Votre nom de famille doit comporter au minimum deux caractères avec seulement des lettres")
+    formData[1].setAttribute("data-error", "Votre nom de famille doit comporter au minimum deux caractères avec seulement des lettres.")
+    validateForm[1]= false;
+  }
+}
+
+//Function validate last name
+function validateEmail() {
+  if (regexEmail.test(emailInput.value)) {
+    formData[2].setAttribute("data-error-visible", "false");
+    formData[2].removeAttribute("data-error");
+    validateForm[2]= true;
+  } else {
+    formData[2].setAttribute("data-error-visible", "true");
+    formData[2].setAttribute("data-error", "Votre e-mail doit être saisie dans un format valide.");
+    validateForm[2]= false;
+  }
+}
+
+//Function validate birthday
+function validateBirthday() {
+  if (regexDate.test(birthdayInput.value)) {
+    formData[3].setAttribute("data-error-visible", "false");
+    formData[3].removeAttribute("data-error");
+    validateForm[3]= true;
+  } else {
+    formData[3].setAttribute("data-error-visible", "true");
+    formData[3].setAttribute("data-error", "Votre date de naissance doit être saisie dans un format valide.");
+    validateForm[3]= false;
   }
 }
 
 //Function validate number tournaments
 function validateNumberTournaments() {
-  if (numberTournamentsInput.value == numberTournamentsInput.value.match(regexNumber)) {
+  if (regexNumber.test(numberTournamentsInput.value)) {
     formData[4].setAttribute("data-error-visible", "false");
     formData[4].removeAttribute("data-error");
+    validateForm[4]= true;
   } else {
     formData[4].setAttribute("data-error-visible", "true");
-    formData[4].setAttribute("data-error", "Le nombre de tournois doit seulement être en chiffre");
+    formData[4].setAttribute("data-error", "Le nombre de tournois doit seulement être en chiffre.");
+    validateForm[4]= false;
+  }
+}
+
+//Function validate participation tournament
+function validateTournamentParticipation() {
+  for(var i=0; i<tournamentParticipationInput.length;i++){
+    if(tournamentParticipationInput[i].checked == true){
+      formData[5].setAttribute("data-error-visible", "false");
+      formData[5].removeAttribute("data-error");
+      validateForm[5]= true;
+      break;
+    }
+    else {
+      formData[5].setAttribute("data-error-visible", "true");
+      formData[5].setAttribute("data-error", "Veuillez sélectionner un tournois auquel participer.");
+      validateForm[5]= false;
+    }
+  }
+}
+
+//Function validate participation tournament
+function validateUseTerm() {
+  if (useTermInput.checked) {
+    formData[6].setAttribute("data-error-visible", "false");
+    formData[6].removeAttribute("data-error");
+    validateForm[6]= true;
+  } else {
+    formData[6].setAttribute("data-error-visible", "true");
+    formData[6].setAttribute("data-error", "Vous devez accepter les conditions d'utilisations.");
+    validateForm[6]= false;
+  }
+}
+
+function validate(event) {
+  console.log(event);
+  event.preventDefault();
+  if (validateForm.every(value => value === true)) {
+    validateForm.style.display= "none";
+    const modalBody = document.querySelector(".modal-body");
+    const heightModal = modalBody.offsetHeight;
+    modalBody.innerHTML = "<p class='confirmation_inscription'>Merci pour votre inscription</p>";
+    modalBody.style.height = heightModal + 'px';
+  } else {
+    console.log("Hum... quelque chose cloche...")
   }
 }
