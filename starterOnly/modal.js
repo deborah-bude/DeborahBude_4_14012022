be4e8e5e1d5d079fa40eb0daf20f8ecd97a4496c
skiro <deborah.bude@gmail.com>
2022-01-28 10:37:30 +0100

    Amélioration fonction validate() pour reset le formulaire et vérifier les erreurs + amélioration responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,7 +14,7 @@ const validateForm = {
   birthdate: false,
   quantity: false,
   location: false,
-  cgu: true
+  cgu: true,
 }
 
 const errorMessages = {
@@ -22,9 +22,17 @@ const errorMessages = {
   last: "Votre nom doit comporter au minimum deux caractères avec seulement des lettres.",
   email: "Votre e-mail doit être saisie dans un format valide.",
   birthdate: "Votre date de naissance doit être saisie dans un format valide.",
-  quantity: "Le nombre de tournois doit seulement être en chiffre.",
+  quantity: "Le nombre de tournois doit seulement être en chiffre et compris entre 0 et 99.",
   location: "Veuillez sélectionner un tournois auquel participer.",
-  cgu: "Vous devez accepter les conditions d'utilisations."
+  cgu: "Vous devez accepter les conditions d'utilisations.",
+}
+
+//Regex for validation
+const dataRegex = {
+  name: /^[a-zA-Z]{2,}$/,
+  email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+  date: /^[0-9]{4}(\-)[0-1][0-9](\-)[0-3][0-9]$/,
+  number: /^[0-9]{1,2}$/,
 }
 
 // DOM Elements
@@ -33,6 +41,7 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
 const form = document.querySelector("#form");
+const submitButton = document.querySelector(".btn-submit");
 //DOM Elements - Input
 const firstNameInput = document.querySelector("#first");
 const lastNameInput = document.querySelector("#last");
@@ -41,23 +50,18 @@ const birthdayInput = document.querySelector("#birthdate");
 const numberTournamentsInput = document.querySelector("#quantity");
 const tournamentParticipationInput = document.getElementsByName("location");
 const useTermInput = document.querySelector("#checkbox1");
-
-const regexName = /^[a-zA-Z]{2,}$/;
-const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-const regexDate = /^[0-9]{4}(\-)[0-1][0-9](\-)[0-3][0-9]$/;
-// const regexDate = /(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))/g;
-const regexNumber = /^[0-9]{1,}$/;
+const validationInscription= document.querySelector(".confirmation_inscription");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 modalCloseBtn.addEventListener("click", closeModal);
 //Validate input
-firstNameInput.addEventListener("input", () => validateField(firstNameInput, regexName));
-lastNameInput.addEventListener("input", () => validateField(lastNameInput, regexName));
-emailInput.addEventListener("input", () => validateField(emailInput, regexEmail));
-birthdayInput.addEventListener("input", () => validateField(birthdayInput, regexDate));
-numberTournamentsInput.addEventListener("input", () => validateField(numberTournamentsInput, regexNumber));
+firstNameInput.addEventListener("input", () => validateField(firstNameInput, "name"));
+lastNameInput.addEventListener("input", () => validateField(lastNameInput, "name"));
+emailInput.addEventListener("input", () => validateField(emailInput, "email"));
+birthdayInput.addEventListener("input", () => validateField(birthdayInput, "date"));
+numberTournamentsInput.addEventListener("input", () => validateField(numberTournamentsInput, "number"));
 for (const location of tournamentParticipationInput) {
   location.addEventListener("input", validateTournamentParticipation);
 }
@@ -67,14 +71,20 @@ useTermInput.addEventListener("input", validateUseTerm);
 function launchModal() {
   modalbg.style.display = "block";
 }
+
 // close modal function
 function closeModal() {
   modalbg.style.display = "none";
+  // form.style.visibility = "visible";
+  // validationInscription.style.visibility = "hidden"
+  form.style.opacity = "1";
+  validationInscription.style.opacity = "0"
 }
 
+// Validation of basic fields witch contain just texts or numbers
 function validateField(input, regex) {
   const name = input.getAttribute('name')
-  if (regex.test(input.value)) {
+  if (dataRegex[regex].test(input.value)) {
     input.parentElement.setAttribute("data-error-visible", "false");
     input.parentElement.removeAttribute("data-error");
     validateForm[name]= true;
@@ -181,21 +191,24 @@ function validateUseTerm() {
   }
 }
 
+// Validation subscription
 function validate(event) {
   console.log(event);
-
   event.preventDefault();
   if (Object.values(validateForm).every(value => value === true)) {
-    //validateForm.style.display= "none";
-    const modalBody = document.querySelector(".modal-body");
-    const heightModal = modalBody.offsetHeight;
-    modalBody.innerHTML = "<p class='confirmation_inscription'>Merci pour votre inscription</p>";
-    modalBody.style.height = heightModal + 'px';
+    // form.style.visibility = "hidden";
+    // validationInscription.style.visibility = "visible";
+    form.style.opacity = "0";
+    validationInscription.style.opacity = "1"
+    form.reset();
   } else {
     console.log("Hum... quelque chose cloche...")
-    
-  validateField(firstNameInput, regexName)
-  validateTournamentParticipation()
-  // Ajouter les autres fonctions ici
+    validateField(firstNameInput, "name");
+    validateField(lastNameInput, "name");
+    validateField(emailInput, "email");
+    validateField(birthdayInput, "date");
+    validateField(numberTournamentsInput, "number");
+    validateTournamentParticipation();
+    validateUseTerm();
   }
 }
